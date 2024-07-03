78232db8b72a91b023a4df1a53e422cbc80a8c2f
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-03 15:20:41 +0530

    modified commit

--- corejavaprojects/src/com/tnsif/scannerclass/Personexce.java
+++ corejavaprojects/src/com/tnsif/scannerclass/Personexce.java
@@ -1,5 +1,26 @@
 package com.tnsif.scannerclass;
+import java.util.Scanner;
 
-public class Personexce {
-
+public class Personexce 
+{
+    public static void main(String[] args) 
+    {
+	    	Scanner sc = new Scanner(System.in);
+	    	System.out.println("Enter the persons name ");
+	    	String name = sc.nextLine();
+	    	System.out.println("Enter person income ");
+	    	int income = sc.nextInt();
+	    	
+	    	Persons_income pp = new Persons_income();
+	    	pp.setName(name);
+	    	pp.setIncome(income);
+	    	
+	    	Taxcalculation t = new Taxcalculation();
+	    	
+	    	t.calculatetax(pp);
+            
+	    	System.out.println("after modification");
+	    	System.out.println(pp);
+	    	
+	}
 }
--- corejavaprojects/src/com/tnsif/scannerclass/Taxcalculation.java
+++ corejavaprojects/src/com/tnsif/scannerclass/Taxcalculation.java
@@ -1,5 +1,14 @@
 package com.tnsif.scannerclass;
 
-public class Taxcalculation {
-
+public class Taxcalculation 
+{
+    public void calculatetax(Persons_income p)
+    {
+    	if(p.getIncome()<=1600)
+    		p.setTax(0);
+    	else if(p.getIncome()>1600 && p.getIncome()<30000)
+    		p.setTax(5);
+    	else
+            p.setTax(10);
+    }
 }
