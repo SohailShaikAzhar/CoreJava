359c9725e2ef6f8c278cb252a26b7d926c3b5588
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-05 18:33:55 +0530

    Bubble sort added

---
+++ corejavaprojects/src/com/tnsif/sort/Bubble_input.java
@@ -0,0 +1,20 @@
+package com.tnsif.sort;
+import java.util.Scanner;
+
+public class Bubble_input 
+{
+    public static void main(String[] args) 
+    {
+    	Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the number of values to sort = ");
+	    int n = sc.nextInt();
+	    System.out.println("Enter the numbers you want to sort = ");
+	    int arr[] = new int[100];
+	    for(int i=0;i<n;i++)
+	    {
+	    	arr[i]= sc.nextInt();
+	    }
+	    Bubble_logic bl = new Bubble_logic();
+	    bl.bubble_alg(arr, n);
+	}
+}
---
+++ corejavaprojects/src/com/tnsif/sort/Bubble_logic.java
@@ -0,0 +1,23 @@
+package com.tnsif.sort;
+
+public class Bubble_logic 
+{
+    public static void bubble_alg(int arr[], int n)
+    {
+    	int temp = 0;
+    	for(int i= 0; i< n; i++)
+    	{
+    		for(int j= 0; j< n; j++)
+    		{
+    			if(arr[i]<arr[j])
+    			{
+    				temp = arr[i];
+    				arr[i] = arr[j];
+    				arr[j] = temp;
+    			}
+    		}
+    	}
+    	Output_bubble ob = new Output_bubble();
+    	ob.display(arr, n);
+    }
+}
