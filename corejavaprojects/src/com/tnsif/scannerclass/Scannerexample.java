2bbd408f8620641e2bbd240bc06244b9af93671e
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-06 19:11:48 +0530

    commit

--- corejavaprojects/src/com/tnsif/scannerclass/Scannerexample.java
+++ corejavaprojects/src/com/tnsif/scannerclass/Scannerexample.java
@@ -1,16 +1,19 @@
 package com.tnsif.scannerclass;
 import java.util.Scanner;
 
-public class Scannerexample {
-    public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter your name ");
-		String name = sc.next();
-		
-		System.out.println("Enter your roll number ");
-		int roll = sc.nextInt();
-		
-		System.out.println("Your name is "+name+"\nYour roll is "+roll);
+public class Scannerexample 
+{
+    public static void main(String[] args) 
+    {
+		try (Scanner sc = new Scanner(System.in)) 
+		{
+			System.out.println("Enter your name ");
+			String name = sc.next();
+			
+			System.out.println("Enter your roll number ");
+			int roll = sc.nextInt();
+			
+			System.out.println("Your name is "+name+"\nYour roll is "+roll);
+		}
 	}
 }
