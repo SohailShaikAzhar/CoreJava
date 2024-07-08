3fa64416336ef7793b3e01a2be6d4212d52ea3a4
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-08 17:55:41 +0530

    inheritance and constructors

--- corejavaprojects/src/com/tnsif/inheritance/Citizen.java
+++ corejavaprojects/src/com/tnsif/inheritance/Citizen.java
@@ -1,5 +1,50 @@
 package com.tnsif.inheritance;
 
-public class Citizen {
-
+public class Citizen 
+{
+    private String name;
+    private long aadhar;
+    private String address;
+    private long phoneno;
+    
+	public Citizen(String name, long aadhar, String address, long phoneno) 
+	{
+		super();
+		this.name = name;
+		this.aadhar = aadhar;
+		this.address = address;
+		this.phoneno = phoneno;
+	}
+	public Citizen() {}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getAadhar() {
+		return aadhar;
+	}
+	public void setAadhar(long aadhar) {
+		this.aadhar = aadhar;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(long phoneno) {
+		this.phoneno = phoneno;
+	}
+	@Override
+	public String toString() {
+		return "Citizen [name=" + name + ", aadhar=" + aadhar + ", address=" + address + ", phoneno=" + phoneno + "]";
+	}
+    
+	
 }
---
+++ corejavaprojects/src/com/tnsif/inheritance/Supermain.java
@@ -0,0 +1,13 @@
+package com.tnsif.inheritance;
+
+public class Supermain 
+{
+    public static void main(String[] args) 
+    {
+	    Student s = new Student("sohail",309234850,"hyderabad",872942383);
+	    System.out.println(s);
+	    
+	    Student sl = new Student(01, "SriIndu");
+	    System.out.println(sl);
+	}
+}
