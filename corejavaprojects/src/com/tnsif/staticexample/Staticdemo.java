c89ee80b5e1627a0f4072b8d936ef3eefd8db346
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-09 11:05:19 +0530

    static variable added

---
+++ corejavaprojects/src/com/tnsif/staticexample/Staticdemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.staticexample;
+
+
+public class Staticdemo 
+{
+    public static void main(String[] args) 
+    {
+	    Employee e1 = new Employee(1, "Shaik");	
+	    Employee e2 = new Employee(2, "Azhar");	
+	    Employee e3 = new Employee(3, "Sohail");
+	    
+	    e1.display();
+	    e2.display();
+	    e3.display();
+	}
+}
