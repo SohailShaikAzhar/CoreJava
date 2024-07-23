df78ff5ab0514423505cd4b3a2ef3d9bb7ca155b
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-24 01:36:27 +0530

    JDBC

--- corejavaprojects/src/com/tnsif/jdbcprograms/Jdbcselect.java
+++ corejavaprojects/src/com/tnsif/jdbcprograms/Jdbcselect.java
@@ -1,9 +1,41 @@
 package com.tnsif.jdbcprograms;
 
 import java.sql.*;
-import javax.sql.*;
-
-public class Jdbcselect 
-{
+import javax.sql.*; 
+public class Jdbcselect {
 	
+	public static void main(String[] args) {
+		
+	// step2: load and register the driver
+	
+	// step 3:Establish connection
+	
+	try {
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/hyd11","sohail","root");
+		
+		// step 4: create statement
+		
+		Statement st=conn.createStatement();
+		
+		// step 5: execute quary
+		
+		String strselect="Select booktitle,price,qty from books";
+		
+		// process the result to display
+		
+		ResultSet rst=st.executeQuery(strselect);
+		System.out.println("The records are");
+		while(rst.next()) {
+			String booktitle=rst.getString("booktitle");
+			int price=rst.getInt("price");
+			int qty=rst.getInt("qty");
+			
+			System.out.println(booktitle+" "+price+" "+qty);
+		}
+		
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+	}
 }
